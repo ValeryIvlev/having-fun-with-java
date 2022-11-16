29617e24e0876405f31085c331f0aa9bdefe35ab
Valery <qaivlev@yandex.ru>
2022-11-16 16:05:24 +0300

    added gradle and Main

---
+++ src/main/java/ru/podeli/Main.java
@@ -0,0 +1,26 @@
+package ru.podeli;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+
+
+        //1. Переполнение
+
+        int d = 1200;
+        byte c = d;
+                System.out.println(c);
+
+
+
+        //2. Попробовать вычисления комбинаций типов данных (int и double)
+
+        int x = 120;
+        double y = 1.1;
+        System.out.println(x+y);
+
+        */
+
+    }
+
+}
