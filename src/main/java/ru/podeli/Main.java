325defd2efc8a27b013bb62f1dc9e17e9141b1a5
Valery <qaivlev@yandex.ru>
2022-11-17 16:48:43 +0300

    UpMain

--- src/main/java/ru/podeli/Main.java
+++ src/main/java/ru/podeli/Main.java
@@ -5,19 +5,35 @@ public class Main {
         /*
 
 
-        //1. Переполнение
+        //1.1 Переполнение
 
         int d = 1200;
         byte c = d;
                 System.out.println(c);
 
+        byte a = -128;
+        byte c = a - 1;
+        System.out.println(c);
+
+        int n = 10000;
+        int x = 2;
+        short c = x;
+        while (n >= (x*x)) {
+            System.out.print(x * x + "!="+c);
+            x++;
+        }
 
 
         //2. Попробовать вычисления комбинаций типов данных (int и double)
 
         int x = 120;
         double y = 1.1;
+
         System.out.println(x+y);
+        System.out.println(200+2000.2);
+        System.out.println(x/y);
+        System.out.println(x*y);
+        System.out.println(x%y);
 
         */
 
